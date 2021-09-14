6ff31893b1edabb776a18138ac48ce872bc211fd
Brayahn <brayahnmainah@gmail.com>
2021-09-14 17:13:30 +0300

    routing and tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Micasa from './pages/Micasa';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-screen-md mx-auto pt-20"> 
+ <Micasa /> 
+
     </div>
   );
 }
---
+++ src/pages/Micasa.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Micasa = () => {
+    return (
+        <div className="mb-20">
+<h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900"> React App </h1>
+
+<p className="mx-auto leading-relaxed text-base mb-4">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+</p>
+
+<p className="mx-auto leading-relaxed text-base mb-4">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem 
+Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+</p>
+
+<p className="mx-auto leading-relaxed text-base mb-4">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem  
+Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+</p>
+        </div>
+    );
+};
+
+export default Micasa;
