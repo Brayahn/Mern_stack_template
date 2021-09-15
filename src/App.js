b3b557f5c9d143d313bda5e727cce1c734089dec
Brayahn <brayahnmainah@gmail.com>
2021-09-15 06:05:46 +0300

    added Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Micasa from './pages/Micasa';
+import Micasa from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NavBar from './components/NavBar';
+
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
+    <NavBar/>
     <div className="max-w-screen-md mx-auto pt-20"> 
- <Micasa /> 
-
+      <Route exact path="/" component={Micasa} />
+      <Route exact path="/About" component={About} />
+      <Route exact path="/Contact" component={Contact} />
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+               <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">Contact us today</h1>
+        </div>
+    );
+};
+
+export default Contact;
